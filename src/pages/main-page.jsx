e982e64a2b89c13d2474bc4273fa50b8da90dab8
mnmhbbb <mnmhb64@gmail.com>
2025-11-06 00:20:09 +0900

    Test: TEST PR

--- src/pages/main-page.jsx
+++ src/pages/main-page.jsx
@@ -1,3 +1,14 @@
+import GlobalLayout from "@/components/GlobalLayout";
+import { useState } from "react";
+
 export default function MainPage() {
-  return <>hello</>;
+  const [value, setValue] = useState("");
+  const test = "";
+
+  return (
+    <div>
+      hello
+      <div></div>
+    </div>
+  );
 }
