1dffa4943bed7ae6a09825c84dbf7ab12007b2be
BZzzzi <khj23465@naver.com>
2025-11-06 19:43:56 +0900

    Feat: 공통 Header 컴포넌트 구현
    - 반응형 레이아웃 적용
    - 로고 클릭 시 루트 페이지로 이동
    - useLocation()을 활용해 페이지별 버튼 노출 조건 분기
    - useLocation() 사용을 위해 BrowserRouter를 main에서 App을 감싸도록 구조 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { Routes, Route } from "react-router";
 import { GlobalStyle } from "@/styles/global-style";
-import GlobalLayout from "@/components/global-layout";
+import GlobalLayout from "@/components/common/global-layout";
 import TestPage from "@/pages/test-page";
 import MessagePage from "@/pages/message-page";
 
@@ -12,7 +12,6 @@ function App() {
         <Routes>
           <Route element={<GlobalLayout />}>
             <Route path="/" element={<TestPage />} />
-            <Route path="/post/:id/message" element={<MessagePage />} />
           </Route>
         </Routes>
       </BrowserRouter>
