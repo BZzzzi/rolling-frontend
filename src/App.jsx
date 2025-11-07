78ffe9b262d274aec6e95ab4a881127f76155777
BZzzzi <khj23465@naver.com>
2025-11-07 19:59:47 +0900

    Feat: 임시 네비게이션 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router";
 import { GlobalStyle } from "@/styles/global-style";
 import GlobalLayout from "@/components/common/global-layout";
 import TestPage from "@/pages/test-page";
+import TempPage from "@/pages/temp-page";
+import ToastTestPage from "@/pages/toast-test-page";
 
 function App() {
   return (
@@ -9,7 +11,14 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route element={<GlobalLayout />}>
-          <Route path="/" element={<TestPage />} />
+          <Route path="/" element={<TempPage />} />
+          <Route path="/main" element={<TestPage />} />
+          <Route path="/list" element={<TestPage />} />
+          <Route path="/post-user" element={<TestPage />} />
+          <Route path="/post" element={<TestPage />} />
+          <Route path="/message" element={<TestPage />} />
+          <Route path="/test-page" element={<TestPage />} />
+          <Route path="/toast-test-page" element={<ToastTestPage />} />
         </Route>
       </Routes>
     </>
