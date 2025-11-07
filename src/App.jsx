5a9be670443888e48de252d4561dd4bc2c043261
summerlane <punkwagon@gmail.com>
2025-11-07 18:15:27 +0900

    Feat: 메인페이지 UI 구현, 메인 백그라운드 이미지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import { GlobalStyle } from "@/styles/global-style";
 import GlobalLayout from "@/components/global-layout";
 import TestPage from "@/pages/test-page";
+import MainPage from "@/pages/main-page";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route element={<GlobalLayout />}>
             <Route path="/" element={<TestPage />} />
+            <Route path="/main" element={<MainPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
