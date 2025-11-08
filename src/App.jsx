1774516a7e4eb6ee0791bbd9a0b597677f5e7795
summerlane <punkwagon@gmail.com>
2025-11-08 14:04:57 +0900

    Feat: post 페이지 라우터 코드 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { GlobalStyle } from "@/styles/global-style";
 import GlobalLayout from "@/components/global-layout";
 import TestPage from "@/pages/test-page";
 import MainPage from "@/pages/main-page";
+import PostPage from "@/pages/post-page";
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<GlobalLayout />}>
-            <Route path="/" element={<TestPage />} />
-            <Route path="/main" element={<MainPage />} />
+            {/* <Route path="/" element={<TestPage />} /> */}
+            <Route path="/" element={<MainPage />} />
+            <Route path="/post" element={<PostPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
