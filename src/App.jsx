c44811326358c2187c824dd697c8e9b33cd5ad6e
BZzzzi <khj23465@naver.com>
2025-11-17 17:28:53 +0900

    Refactor: 개발 편의를 위한 임시 페이지 및 테스트 페이지 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { Routes, Route } from "react-router";
 import { GlobalStyle } from "@/styles/global-style";
 import GlobalLayout from "@/components/common/global-layout";
-import TestPage from "@/pages/test-page";
 import MessagePage from "@/pages/message-page";
 import MainPage from "@/pages/main-page";
 import ListPage from "@/pages/list-page";
 import PostPage from "@/pages/post-page";
-import TempPage from "@/pages/temp-page";
-import ToastTestPage from "@/pages/toast-test-page";
 import RollingPage from "@/pages/rolling-page";
 
 function App() {
@@ -16,15 +13,12 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route element={<GlobalLayout />}>
-          <Route path="/" element={<TempPage />} />
-          <Route path="/main" element={<MainPage />} />
+          <Route path="/" element={<MainPage />} />
           <Route path="/list" element={<ListPage />} />
           <Route path="/post/:id" element={<RollingPage />} />
           <Route path="/post/:id/edit" element={<RollingPage />} />
           <Route path="/post" element={<PostPage />} />
-          <Route path="/message" element={<MessagePage />} />
-          <Route path="/test" element={<TestPage />} />
-          <Route path="/toast-test" element={<ToastTestPage />} />
+          <Route path="/post/:id/message" element={<MessagePage />} />
         </Route>
       </Routes>
     </>
